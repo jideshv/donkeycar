4edb08fba0941e52d1ccb9253d4e2ea37cd24767
Jidesh Veeramachaneni <jideshv@debugit.com>
2020-10-18 17:45:35 +0800

    Crop top of frame and fix aspect ratio

--- donkeycar/parts/camera.py
+++ donkeycar/parts/camera.py
@@ -132,38 +132,25 @@ class CSICamera(BaseCamera):
     Credit: https://github.com/feicccccccc/donkeycar/blob/dev/donkeycar/parts/camera.py
     gstreamer init string from https://github.com/NVIDIA-AI-IOT/jetbot/blob/master/jetbot/camera.py
     '''
-    def gstreamer_pipeline(self, capture_width=3280, capture_height=2464, output_width=224, output_height=224, framerate=21, flip_method=0) :   
-        return 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=%d, height=%d, format=(string)NV12, framerate=(fraction)%d/1 ! nvvidconv flip-method=%d ! nvvidconv ! video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! videoconvert ! appsink' % (
-                capture_width, capture_height, framerate, flip_method, output_width, output_height)
+    def gstreamer_pipeline(self) :
+        return 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3264, height=2464, format=(string)NV12, framerate=(fraction)21/1 ! nvvidconv flip-method=2 ! nvvidconv ! video/x-raw, width=320, height=240, format=(string)BGRx ! videoconvert ! appsink'
     
-    def __init__(self, image_w=160, image_h=120, image_d=3, capture_width=3280, capture_height=2464, framerate=60, gstreamer_flip=0):
+    def __init__(self):
         '''
         gstreamer_flip = 0 - no flip
         gstreamer_flip = 1 - rotate CCW 90
         gstreamer_flip = 2 - flip vertically
         gstreamer_flip = 3 - rotate CW 90
         '''
-        self.w = image_w
-        self.h = image_h
         self.running = True
         self.frame = None
-        self.flip_method = gstreamer_flip
-        self.capture_width = capture_width
-        self.capture_height = capture_height
-        self.framerate = framerate
 
     def init_camera(self):
         import cv2
 
         # initialize the camera and stream
         self.camera = cv2.VideoCapture(
-            self.gstreamer_pipeline(
-                capture_width =self.capture_width,
-                capture_height =self.capture_height,
-                output_width=self.w,
-                output_height=self.h,
-                framerate=self.framerate,
-                flip_method=self.flip_method),
+            self.gstreamer_pipeline(),
             cv2.CAP_GSTREAMER)
 
         self.poll_camera()
@@ -178,7 +165,7 @@ class CSICamera(BaseCamera):
     def poll_camera(self):
         import cv2
         self.ret , frame = self.camera.read()
-        self.frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        self.frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)[80:240,0:320]
 
     def run(self):
         self.poll_camera()
